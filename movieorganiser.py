e8879a3ec3c2438f1221b55ee3ba4dc9aa03f508
Abhishek Pathak <abhishek.p@flipkart.com>
2015-07-26 17:19:06 +0530

    basic comparator and print functions

--- movieorganiser.py
+++ movieorganiser.py
@@ -1,4 +1,23 @@
 import os
 
-if os.path.isdir("E:\English Movies"):
-	print os.listdir("E:\English Movies")
+NEW_DIR = "E:\English Movies"
+BASE_DIR = "D:\English Movies"
+
+
+def verify(directory_name):
+    return os.path.isdir(directory_name)
+
+
+def get_duplicates(base_dir, new_dir):
+    original_files = os.listdir(base_dir)
+    new_files = os.listdir(new_dir)
+    for filename in new_files:
+        if filename in original_files:
+            print "DUPLICATE : ", filename
+
+
+def main(base_dir, new_dir):
+    can_proceed = verify(base_dir) & verify(new_dir)
+    if can_proceed:
+        duplicate_titles = get_duplicates(base_dir, new_dir)
+        print duplicate_titles
